4f22a787f5a29709ba30952606528c99f69c7223
bombel <eryk.urbanskispoko@gmail.com>
2023-04-08 11:48:16 +0200

    added modal exit by clicking overlay

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const openModalBtn = document.getElementById('js-open-modal');
 const closeModalBtn = document.getElementById('js-close-modal');
 const modal = document.getElementById('js-modal');
+const overlay = document.getElementById('js-overlay');
 
 const addClass = (element, style) => {
     element.classList.add(style);
@@ -19,11 +20,23 @@ const styles = {
 openModalBtn.addEventListener('click', () => {
     addClass(modal, styles.opened);
     addClass(document.body, styles.active);
+    overlay.style.display = 'block';
 })
 
 closeModalBtn.addEventListener('click', () => {
     addClass(modal, styles.closed);
     removeClass(document.body, styles.active);
+    overlay.style.display = '';
+    setTimeout(() => {
+        removeClass(modal, styles.closed);
+        removeClass(modal, styles.opened);
+    }, 100);
+})
+
+overlay.addEventListener('click', () => {
+    addClass(modal, styles.closed);
+    removeClass(document.body, styles.active);
+    overlay.style.display = '';
     setTimeout(() => {
         removeClass(modal, styles.closed);
         removeClass(modal, styles.opened);
@@ -34,6 +47,7 @@ document.addEventListener('keyup', e => {
     if (e.key.toLocaleLowerCase() === "escape") {
         addClass(modal, styles.closed);
         removeClass(document.body, styles.active);
+        overlay.style.display = '';
         setTimeout(() => {
             removeClass(modal, styles.closed);
             removeClass(modal, styles.opened);
